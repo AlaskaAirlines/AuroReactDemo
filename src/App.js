9001149df45556bdd42e378165a622b04917ad23
Dominic Valenciana <Dominic.Valenciana@alaskaair.com>
2019-11-06 08:40:07 -0800

    Now have a working wrapper

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import '@alaskaairux/orion-design-tokens/dist/tokens/CSSTokenProperties.css';
-import '@alaskaairux/ods-button';
+import '@alaskaairux/ods-button/dist/ods-button';
+import Wrapper from './wrapper';
 
-const AlertHello = () => alert('Hello World');
+class OdsButton extends Wrapper {
+  TagName = 'ods-button'
+}
 
 class App extends React.Component {
   buttonRef = React.createRef();
+
+  constructor() {
+    super();
+    this.state = {
+      click: () => alert('message 1'),
+      type: "primary"
+    }
+  }
   componentDidMount() {
-      this.buttonRef.current.buttonCallback = AlertHello;
+      this.buttonRef.current.buttonCallback = this.state.click;
+  }
+
+  changeMessage = () => {
+    this.setState({click: () => alert('message 2'), type: "secondary"});
   }
 
   render(){
@@ -20,8 +35,11 @@ class App extends React.Component {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+          {this.state.type}
           {/*Old way to add a custom event listener in react*/}
           <ods-button ref={this.buttonRef}>Hello World</ods-button>
+          <OdsButton yeet="wow" buttonCallback={this.state.click} buttontype={this.state.type}>Hello World 2 {this.state.type}</OdsButton>
+          <button onClick={this.changeMessage} buttontype={this.state.type}>Change Message </button>
       </header>
     </div>
   );
---
+++ src/wrapper.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class Wrapper extends React.PureComponent {
+  ref = React.createRef();
+  funcProps = {};
+  normalProps = {};
+
+  componentDidMount() {
+    this.mapCallbackProps();
+  }
+
+  componentDidUpdate() {
+    this.mapCallbackProps();
+  }
+
+  // Seperate out props that are function so they can be mapped to callbacks
+  calculateProps() {
+    this.funcProps = {};
+    this.normalProps = {};
+    for(let key in this.props) {
+        if(typeof this.props[key] === 'function') {
+            this.funcProps[key] = this.props[key];
+        } else {
+            this.normalProps[key] = this.props[key];
+        }
+    }
+  }
+
+  // Map function props to custom element properties of same name
+  mapCallbackProps() {
+    for (let key in this.funcProps) {
+        this.ref.current[key] = this.funcProps[key];
+    }
+  }
+
+  render() {
+    this.calculateProps();
+    const TagName = this.TagName;
+    return (
+      <TagName ref={this.ref} {...this.normalProps}>
+        {this.props.children}
+      </TagName>
+    );
+  }
+}
+
+export default Wrapper;
