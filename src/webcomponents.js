2cc00aae6ba14be2a653c1371dde218a972c952a
Geoff Rich <Geoff.Rich@alaskaair.com>
2020-05-19 16:58:39 -0700

    feat: update inputoptions to v2 and refactor demo

--- src/webcomponents.js
+++ src/webcomponents.js
@@ -1,4 +1,5 @@
 /* Import any web components used here */
 import '@alaskaairux/ods-button/dist/auro-button';
-import "@alaskaairux/ods-inputoptions";
-import "@alaskaairux/ods-toast";
+import '@alaskaairux/ods-inputoptions/dist/ods-inputoption';
+import '@alaskaairux/ods-inputoptions/dist/ods-inputoption-checkbox-group';
+import '@alaskaairux/ods-toast';
